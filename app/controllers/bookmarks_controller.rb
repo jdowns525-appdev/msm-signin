f45432e1d6746ecf055843cf92cda50388d286c4
jdowns525 <downsjosseph@gmail.com>
2023-03-20 20:58:23 +0000

    commit

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,10 @@
 class BookmarksController < ApplicationController
   def index
-    matching_bookmarks = Bookmark.all
+    #matching_bookmarks = Bookmark.where({ :user_id => session.fetch(:user_id) })
+
+    @current_user = User.where({ :id => session[:user_id] }).at(0)
+
+    matching_bookmarks = @current_user.bookmarks
 
     @list_of_bookmarks = matching_bookmarks.order({ :created_at => :desc })
 
@@ -8,6 +12,8 @@ class BookmarksController < ApplicationController
   end
 
   def show
+    @current_user = User.where({ :id => session[:user_id] }).at(0)
+
     the_id = params.fetch("path_id")
 
     matching_bookmarks = Bookmark.where({ :id => the_id })
@@ -18,8 +24,10 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @current_user = User.where({ :id => session[:user_id] }).at(0)
+
     the_bookmark = Bookmark.new
-    
+
     the_bookmark.user_id = session.fetch(:user_id)
 
     the_bookmark.movie_id = params.fetch("query_movie_id")
@@ -27,12 +35,18 @@ class BookmarksController < ApplicationController
     if the_bookmark.valid?
       the_bookmark.save
       redirect_to("/bookmarks", { :notice => "Bookmark created successfully." })
+       
+      #this is to have the button to create & destroy BUT stay in the same place
+
+      #redirect_to("/movies/#{the_bookmark.movie_id}", { :notice => "Bookmark deleted successfully."} )
     else
       redirect_to("/bookmarks", { :alert => the_bookmark.errors.full_messages.to_sentence })
     end
   end
 
   def update
+    @current_user = User.where({ :id => session[:user_id] }).at(0)
+
     the_id = params.fetch("path_id")
     the_bookmark = Bookmark.where({ :id => the_id }).at(0)
 
@@ -54,5 +68,6 @@ class BookmarksController < ApplicationController
     the_bookmark.destroy
 
     redirect_to("/bookmarks", { :notice => "Bookmark deleted successfully."} )
+    #redirect_to("/movies/#{the_bookmark.movie_id}", { :notice => "Bookmark deleted successfully."} )
   end
 end
